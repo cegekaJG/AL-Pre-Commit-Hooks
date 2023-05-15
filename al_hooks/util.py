09ae69621010c6ff741c0f70733404b34c4d4820
cegekaJG <120664985+cegekaJG@users.noreply.github.com>
2023-05-15 07:55:50 +0000

    update arguments (#1)

--- al_hooks/util.py
+++ al_hooks/util.py
@@ -1,8 +1,10 @@
 import subprocess
 from typing import Any
 
+
 class CalledProcessError(RuntimeError):
     pass
+
 
 def cmd_output(*cmd: str, retcode: int | None = 0, **kwargs: Any) -> str:
     kwargs.setdefault('stdout', subprocess.PIPE)
@@ -14,14 +16,17 @@ def cmd_output(*cmd: str, retcode: int | None = 0, **kwargs: Any) -> str:
         raise CalledProcessError(cmd, retcode, proc.returncode, stdout, stderr)
     return stdout
 
+
 def added_files() -> set[str]:
     return changed_files('--diff-filter=A')
+
 
 def changed_files(arg: str | None = None) -> set[str]:
     cmd = ('git', 'diff', '--staged', '--name-only')
     if arg is not None:
         cmd += arg
     return set(cmd_output(*cmd).splitlines())
+
 
 def zsplit(s: str) -> list[str]:
     s = s.strip('\0')
